0a5876f97a3c73ecd53dbd7267be07da71c0753c
Emil Skogsund <emil@skogsund.se>
2025-10-03 12:13:29 +0200

    .

--- lore_browse.sh
+++ lore_browse.sh
@@ -2,11 +2,7 @@
 # Interactive lore browser launcher
 
 # Activate virtual environment if it exists
-if [ -d ".venv" ]; then
-    source .venv/bin/activate
-elif [ -d "venv" ]; then
-    source venv/bin/activate
-fi
+source .venv/bin/activate
 
 # Run the TUI
 python3 lore_tui.py "$@"
